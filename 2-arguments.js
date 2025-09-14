913b596ef1e413b366e92c47f57603e6929b3015
Jedidiah Anarfi <janarfi25@gmail.com>
2025-09-14 19:20:04 +0100

    modified arguement

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const numOfArguments = process.argv.length - 2;
+if (numOfArguments === 0)   {
+    console.log("No argument");
+} else if (numOfArguments === 1)   {
+    console.log("Argument found");
+} else  {
+    console.log("Argument found");
+}
