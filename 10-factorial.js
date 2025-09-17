76f1bcc92e077c7c390262bb38ff3b6744de292e
Jedidiah Anarfi <janarfi25@gmail.com>
2025-09-17 01:39:01 +0100

    modified output

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+function factorial(n) {
+    if (isNaN(n) || n <= 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+const args = ProcessingInstruction.argv.slice(2);
+const num = parseInt(args[0]);
+console.log(factorial(num));
